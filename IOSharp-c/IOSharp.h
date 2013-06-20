55904548f5fb8e0af4b44ba6184bb75b70086a95
Gerard <gerard@gsole.cat>
2013-06-20 21:02:42 +0200

    Rollingback IOSharp-c

--- IOSharp-c/IOSharp.h
+++ IOSharp-c/IOSharp.h
@@ -2,8 +2,4 @@
 #define IOSHARP_H_INCLUDED
 #include <stdint.h>
 uint64_t start_polling(int pin);
-
-/* SPI */
-void InternalWriteRead(uint16_t[] writeBuffer, int writeOffset, int writeCount, uint16_t[] readBuffer, int readOffset, int readCount, int startReadOffset);
-void InternalWriteRead(uint8_t[] writeBuffer, int writeOffset, int writeCount, uint8_t[] readBuffer, int readOffset, int readCount, int startReadOffset);
 #endif
